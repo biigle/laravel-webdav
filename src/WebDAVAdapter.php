551362e59b0db7c71ab35a51d3d95bc985ed9c15
Martin Zurowietz <martin@zurowietz.de>
2025-07-04 12:15:08 +0200

    Implement custom WebDAVAdapter that fixes readStream()

---
+++ src/WebDAVAdapter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Biigle\WebDav;
+
+use GuzzleHttp\Client as GuzzleClient;
+use League\Flysystem\PathPrefixer;
+use League\Flysystem\UnableToReadFile;
+use League\Flysystem\WebDAV\WebDAVAdapter as Base;
+use Sabre\DAV\Client as WebADVClient;
+
+class WebDAVAdapter extends Base
+{
+    protected PathPrefixer $prefixer;
+
+    public function __construct(
+        protected WebADVClient $client,
+        protected GuzzleClient $guzzle,
+        string $prefix = '',
+        protected string $visibilityHandling = self::ON_VISIBILITY_THROW_ERROR,
+        protected bool $manualCopy = false,
+        protected bool $manualMove = false,
+    ) {
+        parent::__construct($client, $prefix, $visibilityHandling, $manualCopy, $manualMove);
+        $this->prefixer = new PathPrefixer($prefix);
+    }
+
+    public function readStream(string $path)
+    {
+        $location = $this->encodePath($this->prefixer->prefixPath($path));
+
+        try {
+            $url = $this->client->getAbsoluteUrl($location);
+            $response = $this->guzzle->request('GET', $url, ['stream' => true]);
+            return $response->getBody()->detach();
+        } catch (Throwable $exception) {
+            throw UnableToReadFile::fromLocation($path, $exception->getMessage(), $exception);
+        }
+    }
+}
--- src/WebDavServiceProvider.php
+++ src/WebDavServiceProvider.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace Biigle\WebDav;
 
 use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
-use League\Flysystem\WebDAV\WebDAVAdapter;
 use Sabre\DAV\Client as WebDAVClient;
+use GuzzleHttp\Client as GuzzleClient;
 
 class WebDavServiceProvider extends ServiceProvider
 {
@@ -17,11 +18,29 @@ class WebDavServiceProvider extends ServiceProvider
     public function boot()
     {
         Storage::extend('webdav', function ($app, $config) {
-            $pathPrefix = array_key_exists('pathPrefix', $config) ? $config['pathPrefix'] : null;
+            $pathPrefix = $config['pathPrefix'] ?? null;
 
-            $client = new WebDAVClient($config);
+            $guzzleConfig = [];
+            if (array_key_exists('proxy', $config)) {
+                $guzzleConfig['proxy'] = $config['proxy'];
+            }
 
-            $adapter = new WebDAVAdapter($client, $pathPrefix);
+            if (!empty($config['userName'] ?? false) || !empty($config['password'] ?? false)) {
+                $guzzleConfig['auth'] = [$config['userName'], $config['password']];
+
+                if (!array_key_exists('authType', $config)) {
+                    $config['authType'] = 1;
+                } else if ($config['authType'] === 2) {
+                    array_push($guzzleConfig['auth'], 'digest');
+                }  else if ($config['authType'] === 4) {
+                    array_push($guzzleConfig['auth'], 'ntlm');
+                }
+            }
+
+            $webdavClient = new WebDAVClient($config);
+            $guzzleClient = new GuzzleClient($guzzleConfig);
+
+            $adapter = new WebDAVAdapter($webdavClient, $guzzleClient, $pathPrefix);
 
             return new FilesystemAdapter(
                 new Filesystem($adapter, $config),
